4a863ad19b9ed51b9df99eb9268af383cf430763
doannamq <doannamq@gmail.com>
2024-07-21 16:55:26 +0700

    Setup env

---
+++ react/src/views/Surveys.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from "react";
+import PageComponent from "../components/PageComponent";
+import { useStateContext } from "../contexts/ContextProvider";
+import SurveyListItem from "../components/SurveyListItem";
+import TButton from "../components/core/TButton";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+
+const Surveys = () => {
+    const { surveys } = useStateContext();
+    console.log(surveys);
+
+    const onDeleteClick = () => {
+        console.log("On delete click");
+    };
+
+    return (
+        <PageComponent
+            title="Survey"
+            button={
+                <TButton color="green" to="/surveys/create">
+                    <PlusCircleIcon className="h-6 w-6 mr-2" />
+                    Create new
+                </TButton>
+            }
+        >
+            <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+                {surveys.map((survey) => (
+                    <SurveyListItem
+                        survey={survey}
+                        key={survey.id}
+                        onDeleteClick={onDeleteClick}
+                    />
+                ))}
+            </div>
+        </PageComponent>
+    );
+};
+
+export default Surveys;
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/csrf-token', function () {
+    return response()->json(['csrf_token' => csrf_token()]);
+});
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
